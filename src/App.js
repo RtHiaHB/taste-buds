6c374c165afbaf32b279fb3b448953f7c2e627c6
Thomas Davidson <tomd1969@gmail.com>
2023-02-12 10:13:09 -0500

    Stubbed out basic routes.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import TBNavBar from './NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <TBNavBar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/locations" element={<Locations />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
