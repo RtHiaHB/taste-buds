5ac3b90bd7d4d6cd0b217fb78eddb44aa8860cdd
Thomas Davidson <tomd1969@gmail.com>
2023-02-13 19:13:31 -0500

    Added signin page.  Still have to add sign-up portion.

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
 const SignIn = () => {
+    const centeredtext = {
+        'paddingLeft': '25%',
+        'paddingRight': '25%',
+    }
+    const bottomspacing = {
+        'paddingBottom': '.5em'
+    }
     return (
-        <div>
+        <div style={centeredtext}>
             <h1>Sign In</h1>
+            <form>
+                <div style={bottomspacing}>
+                    <label htmlFor='username'>Email:&nbsp;</label>
+                    <input type='text' id='username' /> <br />
+                </div>
+                <div style={bottomspacing}>
+                    <label htmlFor='password'>Password:&nbsp;</label>
+                    <input type='password' id='password' /><br />
+                </div>
+                <input type='submit' id='login' value={'Submit'} />
+            </form>
         </div>
     )
 }
