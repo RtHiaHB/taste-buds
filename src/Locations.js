ad381faecb3562d7a5a3c1a982163053eb3a9c92
John Doe <johndoe@email.com>
2023-02-18 18:51:04 -0500

    Location

--- src/Locations.js
+++ src/Locations.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Locations = () => {
     return (
         <div>
-            <h1>LOCATIONS</h1>
+            <h1>LOCATIONS</h1> 
+         <iframe className='Maps' width="873" height="625" id="gmap_canvas" src="https://maps.google.com/maps?q=Ralegh%20nc&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
         </div>
     )
 }
